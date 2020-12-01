345f086b3e4fb66805f6891efb7e6df165a57441
Chris Lishka <chris.lishka@gmail.com>
2020-11-30 22:17:11 -0800

    Remove obsolete import of matplotlib

--- cnn-cifar10/cnn-cifar10-train.py
+++ cnn-cifar10/cnn-cifar10-train.py
@@ -6,7 +6,6 @@
 import tensorflow as tf
 
 from tensorflow.keras import datasets, layers, models
-import matplotlib.pyplot as plt
 
 (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 
